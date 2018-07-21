7911476ca88f15ff7d018be5fa19dda822f59666
Kirill Kozlov <kozlov.k.e@gmail.com>
2018-07-21 15:22:22 +0200

    Replace synchronized with ReadWriteLock

--- src/main/java/com/github/yuppieflu/stats/service/bucket/StatBucket.java
+++ src/main/java/com/github/yuppieflu/stats/service/bucket/StatBucket.java
@@ -2,39 +2,57 @@ package com.github.yuppieflu.stats.service.bucket;
 
 import com.github.yuppieflu.stats.service.domain.Measurement;
 
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 class StatBucket {
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
     private long count;
     private double max;
     private double min;
     private double sum;
     private long lastAddedTimestamp;
 
-    synchronized void addMeasurement(Measurement m) {
-        if (m.getTimestamp() - lastAddedTimestamp > BucketsStorageService.ONE_BUCKET_MILLIS) {
-            count = 1;
-            max = m.getValue();
-            min = m.getValue();
-            sum = m.getValue();
-        } else {
-            count++;
-            max = Math.max(max, m.getValue());
-            min = Math.min(min, m.getValue());
-            sum += m.getValue();
+    void addMeasurement(Measurement m) {
+        readWriteLock.writeLock().lock();
+        try {
+            if (m.getTimestamp() - lastAddedTimestamp > BucketsStorageService.ONE_BUCKET_MILLIS) {
+                count = 1;
+                max = m.getValue();
+                min = m.getValue();
+                sum = m.getValue();
+            } else {
+                count++;
+                max = Math.max(max, m.getValue());
+                min = Math.min(min, m.getValue());
+                sum += m.getValue();
+            }
+            lastAddedTimestamp = m.getTimestamp();
+        } finally {
+            readWriteLock.writeLock().unlock();
         }
-        lastAddedTimestamp = m.getTimestamp();
-
     }
 
-    synchronized boolean shouldSkip(long timestamp) {
-        return count == 0 || timestamp - lastAddedTimestamp > BucketsStorageService.BUCKETS_SIZE_MILLIS;
+    boolean shouldSkip(long timestamp) {
+        readWriteLock.readLock().lock();
+        try {
+            return count == 0 || timestamp - lastAddedTimestamp > BucketsStorageService.BUCKETS_SIZE_MILLIS;
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
     }
 
-    synchronized StatBucketAccumulator toAccumulator() {
-        return StatBucketAccumulator.builder()
-                                    .count(this.count)
-                                    .max(this.max)
-                                    .min(this.min)
-                                    .sum(this.sum)
-                                    .build();
+    StatBucketAccumulator toAccumulator() {
+        readWriteLock.readLock().lock();
+        try {
+            return StatBucketAccumulator.builder()
+                                        .count(this.count)
+                                        .max(this.max)
+                                        .min(this.min)
+                                        .sum(this.sum)
+                                        .build();
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
     }
 }
